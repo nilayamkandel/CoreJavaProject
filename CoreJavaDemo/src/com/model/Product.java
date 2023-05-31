d204ce7ebebfc95e42675b1a96c05b6c430f876d
neelayam kandel <neelayamdottkandel@gmail.com>
2023-05-31 09:23:07 +0545

    add: Model of Project

---
+++ CoreJavaDemo/src/com/model/Product.java
@@ -0,0 +1,53 @@
+package com.model;
+
+public class Product {
+	private  int id;
+	private String name;
+	private String company;
+	private int price;
+	
+//default constructor 	
+	public Product() {}
+	
+// generated constructor	
+	public Product(int id, String name, String company, int price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.company = company;
+		this.price = price;
+	}
+	
+// generated getter setter
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+//generated to string
+	@Override
+	public String toString() {
+		return "\n Product [id=" + id + ", name=" + name + ", company=" + company + ", price=" + price + "]";
+	}
+	
+}
